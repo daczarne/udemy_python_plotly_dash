9c577917f1cf35ae92861c2439ea3cb1f6e31a4a
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 13:41:20 -0300

    callback slider

--- 08_case_study_creating_an_interactive_map_dashboard/electricity.py
+++ 08_case_study_creating_an_interactive_map_dashboard/electricity.py
@@ -12,18 +12,6 @@ ELECTRICITY = pd.read_csv("electricity.csv")
 MIN_YEAR = ELECTRICITY["Year"].min()
 MAX_YEAR = ELECTRICITY["Year"].max()
 
-avg_price_electricity = ELECTRICITY.groupby(
-    "US_State")["Residential Price"].mean().reset_index()
-
-map_fig = px.choropleth(
-    avg_price_electricity,
-    locations="US_State",
-    locationmode="USA-states",
-    color="Residential Price",
-    scope="usa",
-    color_continuous_scale="reds"
-)
-
 # Instanciate app
 app = dash.Dash(external_stylesheets=[dbc.themes.SOLAR])
 
@@ -38,8 +26,7 @@ app.layout = html.Div([
         marks={i: str(i) for i in range(MIN_YEAR, MAX_YEAR + 1)}
     ),
     dcc.Graph(
-        id="map-graph",
-        figure=map_fig
+        id="map-graph"
     ),
     dash_table.DataTable(
         id="price-info",
@@ -49,6 +36,32 @@ app.layout = html.Div([
 ])
 
 # Callbacks
+
+
+@app.callback(
+    Output(component_id="map-graph", component_property="figure"),
+    Input(component_id="year-slider", component_property="value"),
+)
+def update_map_graph(selected_years):
+    # Filter the data
+    filtered_electricity = ELECTRICITY[
+        (ELECTRICITY["Year"] >= selected_years[0])
+        & (ELECTRICITY["Year"] <= selected_years[1])
+    ]
+    # Compute the avg
+    avg_price_electricity = filtered_electricity.groupby("US_State")[
+        "Residential Price"].mean().reset_index()
+    # Build the map
+    map_fig = px.choropleth(
+        avg_price_electricity,
+        locations="US_State",
+        locationmode="USA-states",
+        color="Residential Price",
+        scope="usa",
+        color_continuous_scale="reds"
+    )
+    # Return the map
+    return map_fig
 
 
 # Run app
