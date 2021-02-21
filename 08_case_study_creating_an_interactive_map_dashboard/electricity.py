93371c27275394cf6c379726e6115f3e45d6adc7
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2021-02-21 13:18:22 -0300

    choropleth

--- 08_case_study_creating_an_interactive_map_dashboard/electricity.py
+++ 08_case_study_creating_an_interactive_map_dashboard/electricity.py
@@ -3,11 +3,24 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 import pandas as pd
+import plotly.express as px
 
 # Global variables
 ELECTRICITY = pd.read_csv("electricity.csv")
 MIN_YEAR = ELECTRICITY["Year"].min()
 MAX_YEAR = ELECTRICITY["Year"].max()
+
+avg_price_electricity = ELECTRICITY.groupby(
+    "US_State")["Residential Price"].mean().reset_index()
+
+map_fig = px.choropleth(
+    avg_price_electricity,
+    locations="US_State",
+    locationmode="USA-states",
+    color="Residential Price",
+    scope="usa",
+    color_continuous_scale="reds"
+)
 
 # Instanciate app
 app = dash.Dash()
@@ -21,6 +34,10 @@ app.layout = html.Div([
         max=MAX_YEAR,
         value=[MIN_YEAR, MAX_YEAR],
         marks={i: str(i) for i in range(MIN_YEAR, MAX_YEAR + 1)}
+    ),
+    dcc.Graph(
+        id="map-graph",
+        figure=map_fig
     )
 ])
 
